bbabc0330b77bae25233e9c8d673f4d697620bae
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-05-13 18:45:37 -0300

    Validacion en UI

--- js/app.js
+++ js/app.js
@@ -7,6 +7,13 @@ const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 
+// Campos Obligatorios
+const campos = {
+    email: false,
+    asunto: false,
+    mensaje: false
+};
+
 // Eventos
 eventListeners();
 
@@ -27,19 +34,62 @@ function iniciarApp() {
 }
 
 // Valida el formulario
-function validarFormulario(e){
+function validarFormulario(e){ 
+    // Evaluar Campo Vacio   
     if(e.target.value.length > 0){
-        console.log('Si hay algo');
-    }else{
-        e.target.classList.add('border', 'border-red-500');
+        // Elimina los errores...
+        const error = document.querySelector('p.error');
 
-        mostrarError();
+        if(error){
+            error.remove();
+        }
+            
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
+    }else{
+        campos[e.target.id] = false;
+        e.target.classList.remove('border', 'border-green-500');
+        e.target.classList.add('border', 'border-red-500');
+        mostrarError('Todos lo campos son obligatorios');
+        return false;
     }
+
+    switch(e.target.id){
+        case 'email':
+            const regExEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+            const valorEmail = e.target.value;
+
+            if(regExEmail.test(valorEmail)){
+                campos[e.target.id] = true;
+                e.target.classList.remove('border', 'border-red-500');
+                e.target.classList.add('border', 'border-green-500');
+            }else{
+                campos[e.target.id] = false;
+                e.target.classList.remove('border', 'border-green-500');
+                e.target.classList.add('border', 'border-red-500');
+            }
+
+            break;
+        default:
+            campos[e.target.id] = true;
+            break;
+    }
+
+    // Evaluar si se habilita el boton
+    const { email, asunto, mensaje } = campos;
+    
+    if(email && asunto && mensaje){
+        
+    }
+
+    console.log(errores);
+    
 }
 
-function mostrarError(){
+function mostrarError(mensaje){
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.textContent = mensaje;
     mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
 
     const errores = document.querySelectorAll('.error');
